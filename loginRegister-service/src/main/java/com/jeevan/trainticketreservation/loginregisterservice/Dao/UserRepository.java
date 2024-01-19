cd4f3c669faecc179da33b1948a1856127e27f0e
jeevanthecoder <98173940+jeevanthecoder@users.noreply.github.com>
2024-01-19 23:31:27 +0530

    Add files via upload

---
+++ loginRegister-service/src/main/java/com/jeevan/trainticketreservation/loginregisterservice/Dao/UserRepository.java
@@ -0,0 +1,13 @@
+package com.jeevan.trainticketreservation.loginregisterservice.Dao;
+
+import com.jeevan.trainticketreservation.loginregisterservice.Entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Users,Integer> {
+
+    public Users findUsersByUserName(String userName);
+
+}
