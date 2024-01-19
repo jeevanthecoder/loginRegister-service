bb29698996e559dc069ce746d21146b241df6074
jeevanthecoder <98173940+jeevanthecoder@users.noreply.github.com>
2024-01-19 20:35:33 +0530

    Add files via upload

---
+++ loginRegister-service/src/main/java/com/jeevan/trainticketreservation/loginregisterservice/Entity/Reservation.java
@@ -0,0 +1,50 @@
+package com.jeevan.trainticketreservation.loginregisterservice.Entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.Set;
+
+@Table
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation {
+
+    @Id
+    @GeneratedValue
+    public int rId;
+
+    @Column(unique = true)
+    public Long pnrNumber;
+
+
+    public String trainNumber;
+
+    public String trainName;
+
+
+    public String source;
+    public String dest;
+
+    public Date dateOfJourney;
+
+    String classtype;
+
+    public Long numberOfSeats;
+
+    public int totalCost;
+
+
+    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JsonBackReference
+    public Users users;
+}
