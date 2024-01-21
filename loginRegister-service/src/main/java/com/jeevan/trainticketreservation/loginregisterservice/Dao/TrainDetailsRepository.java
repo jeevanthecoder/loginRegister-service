a1e3597f1d3d2ccce0eeaf45a29a65298757340b
jeevanthecoder <98173940+jeevanthecoder@users.noreply.github.com>
2024-01-21 11:21:19 +0530

    Delete loginRegister-service directory

--- loginRegister-service/src/main/java/com/jeevan/trainticketreservation/loginregisterservice/Dao/TrainDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jeevan.trainticketreservation.loginregisterservice.Dao;
-
-import com.jeevan.trainticketreservation.loginregisterservice.Entity.TrainDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrainDetailsRepository extends JpaRepository<TrainDetails,Integer> {
-
-    public TrainDetails findTrainDetailsByTrainNumber(String trainNumber);
-}
